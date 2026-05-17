3e9694e167922fc4cc0e42f891f09538e4e6ec25
Abeeb Adesina <deolabeeb@gmail.com>
2026-05-17 14:53:13 +0100

    Install Codespaces DevTools libraries

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 python -m pip install --upgrade pip
 sudo apt-get update
-sudo apt-get install -y libdbus-1-3
+sudo apt-get install -y libatk-bridge2.0-0 libatk1.0-0 libdbus-1-3 libgtk-3-0 libnss3 libxss1
 python -m pip install -r apps/api/requirements-dev.txt
 npm install
 
